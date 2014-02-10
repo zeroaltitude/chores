10feb5df5cf8148eb69b9fa481e9446b7655914c
Eddie Abrams <eabrams@synapdx.com>
2014-02-09 19:56:24 -0500

    admin interface and users added

--- chores/minder/admin.py
+++ chores/minder/admin.py
@@ -1,20 +1,33 @@
 from django.contrib import admin
 
 # Register your models here.
-from chores.minder.models import Chore, ChoreOwner, ChoreCompleted 
+from minder.models import Chore, ChoreOwner, ChoreCompleted 
+from django import forms
+
+
+class ChoreChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.chore_name
+
+
+class ChoreOwnerForm(forms.ModelForm):
+    chore = ChoreChoiceField(queryset=Chore.objects.all())
+    class Meta:
+        model = Chore
 
 
 class ChoreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('chore_name', 'chore_value',) 
 admin.site.register(Chore, ChoreAdmin)
 
 
 class ChoreOwnerAdmin(admin.ModelAdmin):
-    pass
+    form = ChoreOwnerForm
+    list_display = ('chore', 'owner',)
 admin.site.register(ChoreOwner, ChoreOwnerAdmin)
 
 
 class ChoreCompletedAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('chore', 'complete_date',)
 admin.site.register(ChoreCompleted, ChoreCompletedAdmin)
 
